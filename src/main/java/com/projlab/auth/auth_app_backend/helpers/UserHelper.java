a923e65ca3b9c94f83eeadcd2294b47c4a81556d
surac <sshresth147@gmail.com>
2026-03-28 11:09:20 -0500

    User API completed

---
+++ src/main/java/com/projlab/auth/auth_app_backend/helpers/UserHelper.java
@@ -0,0 +1,11 @@
+package com.projlab.auth.auth_app_backend.helpers;
+
+import java.util.UUID;
+
+public class UserHelper {
+
+    public static UUID parseUUID(String uuid){
+
+        return UUID.fromString(uuid);
+    }
+}
